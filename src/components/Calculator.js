95950d553a7be2440967c6923b7d6ce3ab4882de
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-05 19:11:45 +0100

    Use calulator js logic in Calculator js component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,74 +1,98 @@
-import './Calculator.css';
 import React from 'react';
-
+import './Calculator.css';
+import calculate from '../logic/calculate';
+('../logic/calculate');
+import operate from '../logic/operate';
+('../logic/operate');
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const buttonName = event.target.textContent;
+    const calculator = calculate(this.state, buttonName);
+    this.setState(calculator);
   }
 
   render() {
+    const {next, total, operation} = this.state;
+
     return (
       <div className="calculator-grid">
         <div className="output">
           <div data-previous-operand className="previous-operand" />
-          <div data-current-operand className="current-operand">0</div>
+          <div data-current-operand className="current-operand">
+            {next || total || '0'}
+          </div>
         </div>
-        <button data-all-clear className="btn btn-white" type="button">
+        <button
+          data-all-clear
+          className="btn btn-white"
+          type="button"
+          onClick={this.handleClick}
+        >
           AC
         </button>
-        <button data-delete className="btn btn-white" type="button">
+        <button data-delete className="btn btn-white" type="button" onClick={this.handleClick}>
           +/-
         </button>
-        <button data-delete className="btn btn-white" type="button">
+        <button data-delete className="btn btn-white" type="button" onClick={this.handleClick}>
           %
         </button>
-        <button data-operation className="btn btn-orange" type="button">
+        <button data-operation className="btn btn-orange" type="button" onClick={this.handleClick}>
           ÷
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           7
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           8
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           9
         </button>
-        <button data-operation className="btn btn-orange" type="button">
+        <button data-operation className="btn btn-orange" type="button" onClick={this.handleClick}>
           x
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           4
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           5
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           6
         </button>
-        <button data-operation className="btn btn-orange" type="button">
+        <button data-operation className="btn btn-orange" type="button" onClick={this.handleClick}>
           -
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           1
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           2
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           3
         </button>
-        <button data-operation className="btn btn-orange" type="button">
+        <button data-operation className="btn btn-orange" type="button" onClick={this.handleClick}>
           +
         </button>
-        <button data-number className="span-two btn btn-white" type="button">
+        <button data-number className="span-two btn btn-white" type="button" onClick={this.handleClick}>
           0
         </button>
-        <button data-number className="btn btn-white" type="button">
+        <button data-number className="btn btn-white" type="button" onClick={this.handleClick}>
           .
         </button>
-        <button data-equals className="btn btn-orange btn--animated" type="button">
+        <button
+          data-equals
+          className="btn btn-orange btn--animated"
+          type="button"
+          onClick={this.handleClick}
+        >
           =
         </button>
       </div>
