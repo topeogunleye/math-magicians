c9ca84c47e2910be3dc908c96295371482d5939f
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-11 16:48:29 +0100

    Update quote page

---
+++ src/pages/quote/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Quote.css';
+
+function Quote() {
+  return (
+    <div className="quote-container">
+      “mathematics is not about numbers, equations, computations or algorithms;
+      it’s about understanding.”
+    </div>
+  );
+}
+
+export default Quote;
