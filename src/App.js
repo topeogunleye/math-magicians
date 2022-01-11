953bcce307f1504e9094235ca728ad98293a14ce
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-11 16:45:31 +0100

    Add Router and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { Fragment } from 'react';
-import Home from './pages/home/Home.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import CalculatorPage from './pages/calculator/CalculatorPage';
+import Home from './pages/home/Home';
+import Quote from './pages/quote/Quote';
 
 function App() {
   return (
-    <Fragment>
-      <Home />
-    </Fragment>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </Router>
   );
 }
 
