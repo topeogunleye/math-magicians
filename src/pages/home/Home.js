811897691f2a68dd9b898268acfeee323f665ccd
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-11 16:48:03 +0100

    Update Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import Calculator from '../../components/calculator/Calculator';
 import './Home.css';
 
 function Home() {
   return (
-    <div className="homepage">
-      <h1>Lets Do Some Math</h1>
-      <Calculator />
+    <div className="home-container">
+      <h1>Welcome to our page</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolores
+        aperiam accusamus, optio dolorum doloremque earum dolor omnis sit
+        officiis facere placeat hic saepe reiciendis quidem. Laborum dolores
+        adipisci quaerat optio provident nemo commodi assumenda eligendi, quod
+        ad consequuntur reiciendis natus quas, labore earum? Illo impedit omnis
+        eum doloremque cumque.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nesciunt
+        aliquam temporibus qui alias accusantium iste, magni adipisci facilis
+        quod at deleniti ratione aut natus deserunt inventore officiis, ducimus
+        odit omnis. Eaque, molestiae vel dignissimos quo voluptates, quaerat
+        maxime hic magnam vero id recusandae doloribus nemo architecto enim
+        beatae veritatis!
+      </p>
     </div>
   );
 }
