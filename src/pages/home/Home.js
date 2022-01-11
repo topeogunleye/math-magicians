d08ece1c941f7311747df59fa83a12f3c796b8a3
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-11 11:24:14 +0100

    Move Home component and style into it's own  folder

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calculator from '../../components/calculator/Calculator'
+
+function Home() {
+  return (
+    <div>
+      <h1>Lets Do Some Math</h1>
+      <Calculator/>
+    </div>
+  )
+}
+
+export default Home
